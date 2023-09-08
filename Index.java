fa4b472c5d54096c52a072008c398e448201ca1c
Sophia Zhang <ssz.windyskies@gmail.com>
2023-09-08 13:16:09 -0700

    index: init
    i'm pretty sure it works!!

--- Index.java
+++ Index.java
@@ -8,19 +8,35 @@ import java.util.HashMap;
 public class Index
 {
     private HashMap <String, String> ind;
-    File indexFile;
 
     public Index ()
     {
         //some constructor ig
         ind = new HashMap <String, String> ();
-        indexFile = new File ("index");
+
     }
 
     //initializes a project
-    public void init (String pName) //pName = project name
+    public void init ()
     {
-        
+        //new code
+        File f = new File ("objects");
+
+        //if directory doesn't exist, make a new one
+        if (!f.exists() || !f.isDirectory ())
+        {
+            String dirName = "objects"; /* something to pull specified dir from input */;
+            File dir = new File (dirName);
+            dir.mkdirs();
+        }
+
+        File in = new File ("objects");
+
+        //if index doesn't exist, make a new one
+        if (!in.exists())
+        {
+            in = new File ("objects", "index");
+        }
     }
 
     public void addBlob (String fileName) throws IOException
@@ -31,7 +47,7 @@ public class Index
 
         ind.put (ogName, sha1);
 
-        PrintWriter pw = new PrintWriter (indexFile);
+        PrintWriter pw = new PrintWriter ("index");
 
         //updates the entire index... a little excessive, but it works
         for (HashMap.Entry <String, String> entry : ind.entrySet ())
@@ -50,7 +66,7 @@ public class Index
     {
         ind.remove (fileName);
 
-        PrintWriter pw = new PrintWriter (indexFile);
+        PrintWriter pw = new PrintWriter ("index");
         //updates the entire index... a little excessive, but it works
         for (HashMap.Entry <String, String> entry : ind.entrySet ())
             {
@@ -63,9 +79,10 @@ public class Index
     public static void main (String [] args) throws IOException
     {
         Index testList = new Index ();
+        testList.init ();
         testList.addBlob ("testingFile.txt");
-        testList.addBlob ("test2.txt");
-        testList.removeBlob ("testingFile.txt");
+        //testList.addBlob ("test2.txt");
+        //testList.removeBlob ("testingFile.txt");
         //testList.removeBlob ("test2.txt");
     }
 
